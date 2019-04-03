8d00204144af442882d424a860fbded9468b3607
WickiesLee <crowdrider1@gmail.com>
2019-04-03 12:25:54 +0900

    깃허브 주소 추가
    완료

--- frontend/src/components/etc/Contact.vue
+++ frontend/src/components/etc/Contact.vue
@@ -3,6 +3,12 @@
             <h2 class="contact-maintxt">CREDITS</h2>
             <hr class="contact-hr-grow">
             <Credits class="contact-credits"/>
+            <br/>
+            <div style="text-align:center">
+            <h1>Source Code</h1>
+            <a href="https://github.com/WickiesLee/owl-front-server" target="_blank"><h3>https://github.com/WickiesLee/owl-front-server</h3><br/></a>
+            <a href="https://github.com/WickiesLee/owl-front-server" target="_blank"><h3>https://github.com/WickiesLee/owl-rest-server</h3><br/></a>
+            </div>
         </div>
 </template>
 
